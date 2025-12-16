3f5a10d9d1a82d5ceab6e06e125efc9b2526c7ff
kastashenkova <astashenkova.katya@gmail.com>
2025-12-16 02:11:31 +0200

    clean up

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -10,43 +10,16 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     @Override
     public boolean test(Candidate candidate) {
-        return validateAge().test(candidate)
-                && validateVote().test(candidate)
-                && validateNationality().test(candidate)
-                && validatePeriodsInUkr().test(candidate);
-    }
-
-    @Override
-    public Predicate<Candidate> and(Predicate<? super Candidate> other) {
-        return Predicate.super.and(other);
-    }
-
-    @Override
-    public Predicate<Candidate> negate() {
-        return Predicate.super.negate();
-    }
-
-    @Override
-    public Predicate<Candidate> or(Predicate<? super Candidate> other) {
-        return Predicate.super.or(other);
-    }
-
-    public static Predicate<Candidate> validateAge() {
-        return p -> p.getAge() >= REQUIRED_AGE;
-    }
-
-    public static Predicate<Candidate> validateVote() {
-        return Candidate::isAllowedToVote;
-    }
-
-    public static Predicate<Candidate> validateNationality() {
-        return p -> p.getNationality().equals(REQUIRED_NATIONALITY);
-    }
-
-    public static Predicate<Candidate> validatePeriodsInUkr() {
-        return p -> {
-            String[] parts = p.getPeriodsInUkr().split("-");
-            return Integer.parseInt(parts[1]) - Integer.parseInt(parts[0]) >= REQUIRED_YEARS_IN_UKR;
-        };
+        if (candidate.getAge() < REQUIRED_AGE) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        if (!candidate.getNationality().equals(REQUIRED_NATIONALITY)) {
+            return false;
+        }
+        String[] parts = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(parts[1]) - Integer.parseInt(parts[0]) >= REQUIRED_YEARS_IN_UKR;
     }
 }
